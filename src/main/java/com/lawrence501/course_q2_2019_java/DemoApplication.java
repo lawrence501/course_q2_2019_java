fbf78af15fc25d0e5eec7a64d36ce40e9f94f956
macdonl4 <macdonl4@anz.com>
2019-05-20 18:44:35 +1000

    Initial commit

---
+++ src/main/java/com/lawrence501/course_q2_2019_java/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.lawrence501.course_q2_2019_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lawrence501/course_q2_2019_java/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lawrence501.course_q2_2019_java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
