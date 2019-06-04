296ff0bcad8fefd4929c1338306568c9a568f919
macdonl4 <macdonl4@anz.com>
2019-06-04 18:50:38 +1000

    Adding exceptions pkg

---
+++ src/main/java/com/lawrence501/course_q2_2019_java/exceptions/ProjectIdException.java
@@ -0,0 +1,13 @@
+package com.lawrence501.course_q2_2019_java.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ProjectIdException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+  public ProjectIdException(String message) {
+    super(message);
+  }
+}
