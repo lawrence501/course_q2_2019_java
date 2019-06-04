904ab9c600dcf09207a1b0303fd813347abd7066
macdonl4 <macdonl4@anz.com>
2019-06-04 18:58:28 +1000

    Session dump

--- src/main/java/com/lawrence501/course_q2_2019_java/web/ProjectController.java
+++ src/main/java/com/lawrence501/course_q2_2019_java/web/ProjectController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import com.lawrence501.course_q2_2019_java.domain.Project;
 import com.lawrence501.course_q2_2019_java.services.ProjectService;
+import com.lawrence501.course_q2_2019_java.services.MapValidationErrorService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,13 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/project")
 public class ProjectController {
   @Autowired
+  private MapValidationErrorService mapValidationErrorService;
+  @Autowired
   private ProjectService projectService;
 
   @PostMapping("")
   public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-    if (result.hasErrors()) {
-      return new ResponseEntity<String>("Invalid project object", HttpStatus.BAD_REQUEST);
-    }
+    ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+    if (errorMap != null) return errorMap;
 
     project = projectService.saveOrUpdateProject(project);
     return new ResponseEntity<Project>(project, HttpStatus.CREATED);
