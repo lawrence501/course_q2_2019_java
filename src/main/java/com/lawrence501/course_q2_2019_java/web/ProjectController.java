3830e33549a7b969229921b3b2381dc2d3bab8f0
macdonl4 <macdonl4@anz.com>
2019-06-17 18:36:46 +1000

    Session of work, finishing section

--- src/main/java/com/lawrence501/course_q2_2019_java/web/ProjectController.java
+++ src/main/java/com/lawrence501/course_q2_2019_java/web/ProjectController.java
@@ -10,7 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +34,25 @@ public class ProjectController {
     if (errorMap != null) return errorMap;
 
     project = projectService.saveOrUpdateProject(project);
-    return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    return new ResponseEntity<Project>(project, HttpStatus.OK);
+  }
+
+  @GetMapping("/{projectId}")
+  public ResponseEntity<?> getProjectById(@PathVariable String projectId) {
+    Project project = projectService.findProjectByIdentifier(projectId);
+
+    return new ResponseEntity<Project>(project, HttpStatus.OK);
+  }
+
+  @GetMapping("/all")
+  public ResponseEntity<?> getAllProjects() {
+    Iterable<Project> projects = projectService.findAllProjects();
+    return new ResponseEntity<Iterable<Project>>(projects, HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{projectId}")
+  public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+    projectService.deleteProjectByIdentifier(projectId);
+    return new ResponseEntity<String>("Project '" + projectId + "' deleted successfully", HttpStatus.OK);
   }
 }
