b68175f9bc9e896ab83e6a8901408effcfbb0346
macdonl4 <macdonl4@anz.com>
2019-08-26 18:03:52 +1000

    Finished off previous session's lecture

--- src/main/java/com/lawrence501/course_q2_2019_java/security/JwtTokenProvider.java
+++ src/main/java/com/lawrence501/course_q2_2019_java/security/JwtTokenProvider.java
@@ -1,17 +1,33 @@
 package com.lawrence501.course_q2_2019_java.security;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.lawrence501.course_q2_2019_java.domain.User;
 import static com.lawrence501.course_q2_2019_java.security.SecurityConstants.EXPIRATION_TIME;
+import static com.lawrence501.course_q2_2019_java.security.SecurityConstants.SECRET;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
 public class JwtTokenProvider {
   public String generateToken(Authentication auth) {
     User user = (User) auth.getPrincipal();
     Date now = new Date(System.currentTimeMillis());
     Date expiryDate = new Date(now.getTime() + EXPIRATION_TIME);
-    
+    String userId = Long.toString(user.getId());
+
+    Map<String, Object> claims = new HashMap<>();
+    claims.put("id", userId);
+    claims.put("username", user.getUsername());
+    claims.put("fullName", user.getFullName());
+
+    return Jwts.builder().setSubject(userId).setClaims(claims).setIssuedAt(now).setExpiration(expiryDate)
+        .signWith(SignatureAlgorithm.HS512, SECRET).compact();
   }
 }
--- src/main/java/com/lawrence501/course_q2_2019_java/web/UserController.java
+++ src/main/java/com/lawrence501/course_q2_2019_java/web/UserController.java
@@ -1,8 +1,13 @@
 package com.lawrence501.course_q2_2019_java.web;
 
+import javax.naming.Binding;
 import javax.validation.Valid;
 
 import com.lawrence501.course_q2_2019_java.domain.User;
+import com.lawrence501.course_q2_2019_java.payload.JwtLoginSuccessResponse;
+import com.lawrence501.course_q2_2019_java.payload.LoginRequest;
+import com.lawrence501.course_q2_2019_java.security.JwtTokenProvider;
+import static com.lawrence501.course_q2_2019_java.security.SecurityConstants.TOKEN_PREFIX;
 import com.lawrence501.course_q2_2019_java.services.MapValidationErrorService;
 import com.lawrence501.course_q2_2019_java.services.UserService;
 import com.lawrence501.course_q2_2019_java.validator.UserValidator;
@@ -10,6 +15,10 @@ import com.lawrence501.course_q2_2019_java.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +36,27 @@ public class UserController {
 
   @Autowired
   private UserValidator userValidator;
+
+  @Autowired
+  private JwtTokenProvider tokenProvider;
+
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
+  @PostMapping("/login")
+  public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+    ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+    if (errorMap != null) {
+      return errorMap;
+    }
+
+    Authentication authentication = authenticationManager
+        .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
+
+    return ResponseEntity.ok(new JwtLoginSuccessResponse(true, jwt));
+  }
 
   @PostMapping("/register")
   public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
